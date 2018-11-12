7873d16d1cfc6b315f62e8dee94e31347812cd6a
springkim <springnode@gmail.com>
2018-11-12 19:29:11 +0900

    rere

---
+++ README.md
@@ -0,0 +1,35 @@
+ceres-windows
+=============
+
+A ceres-solver wrapper for Visual Studio.
+Tested on Visual Studio 2015 and 2017, using x64 on both Debug and Release.
+
+For a wrapper that uses ceres-solver 1.9.0 and CXSparse, go to the
+CXSparse branch at https://github.com/tbennun/ceres-windows/tree/cxsparse
+
+Conversion to newer versions of Visual Studio should be performed automatically by the IDE. 
+If projects are not compiling, make sure that the platform toolset matches the version of Visual
+Studio in the project properties.
+
+
+Sample projects (using ceres-solver's code examples) are also embedded in the solution.
+
+Dependencies
+============
+
+  * Eigen (http://eigen.tuxfamily.org/): Version 3.3 or above. It's a header-only 
+    library, so simply extract Eigen to the solution directory and it should compile. 
+    Alternatively, set it up in one of your default include directories.
+
+Libraries
+=========
+
+  * ceres-solver 1.12.0: http://ceres-solver.org/
+
+  * Google glog: http://www.github.com/google/glog
+
+
+License
+=======
+
+The license of this wrapper is the same as ceres-solver and glog, New BSD license.
---
+++ build_ceres_vs2017.bat
@@ -0,0 +1,70 @@
+@echo off
+set "VSVAR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+set VSNUM=2017
+where /Q hg 2>&1 >NUL
+if %ERRORLEVEL% == 1 (
+	echo Please install mercurial before building ceres_solver.
+	pause
+	exit /b
+)
+
+md 3rdparty\include
+md 3rdparty\lib\x64\Debug
+md 3rdparty\lib\x64\Release
+md 3rdparty\lib\x86\Debug
+md 3rdparty\lib\x86\Release
+md 3rdparty\bin\x64\Debug
+md 3rdparty\bin\x64\Release
+md 3rdparty\bin\x86\Debug
+md 3rdparty\bin\x86\Release
+::Visual Studio 2013
+md 3rdparty\staticlib120\x64\Debug
+md 3rdparty\staticlib120\x64\Release
+md 3rdparty\staticlib120\x86\Debug
+md 3rdparty\staticlib120\x86\Release
+::Visual Studio 2015
+md 3rdparty\staticlib140\x64\Debug
+md 3rdparty\staticlib140\x64\Release
+md 3rdparty\staticlib140\x86\Debug
+md 3rdparty\staticlib140\x86\Release
+::Visual Studio 2017
+md 3rdparty\staticlib141\x64\Debug
+md 3rdparty\staticlib141\x64\Release
+md 3rdparty\staticlib141\x86\Debug
+md 3rdparty\staticlib141\x86\Release
+
+
+
+pushd %cd%
+cd win\include
+call :SafeRMDIR "Eigen"
+call :SafeRMDIR "eigen-temp"
+md eigen-temp
+cd eigen-temp
+hg clone https://bitbucket.org/eigen/eigen/
+move eigen\Eigen ..\
+call :SafeRMDIR "eigen-temp"
+popd
+call "%VSVAR%"
+powershell "[io.path]::GetFileName($(Get-ChildItem 'hklm:\SOFTWARE\WOW6432Node\Microsoft\Windows Kits\Installed Roots').Name)" > sdk.txt
+set /p "sdkver="<"sdk.txt"
+DEL sdk.txt
+msbuild ceres-%VSNUM%.sln /p:Configuration=Debug /p:Platform=x64 /p:PlatformToolset=v141 /p:WindowsTargetPlatformVersion=%sdkver%
+msbuild ceres-%VSNUM%.sln /p:Configuration=Release /p:Platform=x64 /p:PlatformToolset=v141 /p:WindowsTargetPlatformVersion=%sdkver%
+
+set HOME=ceres-windows
+xcopy /Y "ceres-solver\include\*.*" ..\%HOME%\3rdparty\include\ /e /h /k 2>&1 >NUL
+xcopy /Y "ceres-solver\config\ceres\internal\config.h" ..\%HOME%\3rdparty\include\ceres\internal\config.h /e /h /k 2>&1 >NUL
+xcopy /Y "win\include\Eigen\*.*" ..\%HOME%\3rdparty\include\Eigen\ /e /h /k 2>&1 >NUL
+xcopy /Y "x64\Release\ceres.lib" ..\%HOME%\3rdparty\lib\x64\Release 2>&1 >NUL
+xcopy /Y "x64\Release\ceres.dll" ..\%HOME%\3rdparty\bin\x64\Release 2>&1 >NUL
+xcopy /Y "x64\Debug\ceres.lib" ..\%HOME%\3rdparty\lib\x64\Debug 2>&1 >NUL
+xcopy /Y "x64\Debug\ceres.dll" ..\%HOME%\3rdparty\bin\x64\Debug 2>&1 >NUL
+pause
+exit /b
+::::::::::::::::::::::::::::::FUNCTION::::::::::::::::::::::::::::::
+:SafeRMDIR
+IF EXIST "%~1" (
+	RMDIR /S /Q "%~1" 2>&1 >NUL
+)
+exit /b
